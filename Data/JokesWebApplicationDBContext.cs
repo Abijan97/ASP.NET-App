9c43469d8cd8ac04ea36e48e2db586d273f668d2
Abijan97 <padhmath_im17052@stu.kln.ac.lk>
2021-08-09 16:44:49 +0530

    Add project files.

---
+++ Data/JokesWebApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using JokesWebApplication.Models;
+
+namespace JokesWebApplication.Data
+{
+    public class JokesWebApplicationDBContext : DbContext
+    {
+        public JokesWebApplicationDBContext (DbContextOptions<JokesWebApplicationDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<JokesWebApplication.Models.Joke> Joke { get; set; }
+    }
+}
